c3117e6bc39ded4f61ef9e36984e354d65511fb8
NaimSantos <naimsantos45@gmail.com>
2021-11-09 09:11:22 -0300

    Correções e scripts para plotar resultados

--- source/solver.cpp
+++ source/solver.cpp
@@ -12,29 +12,39 @@ template <typename T> void print_array_1D(const std::vector<T> A);
 template <typename T> void print_array_2D(const std::vector<std::vector<T>> A);
 template <typename T> std::vector<T> linspace(const double xi, const double xf, int Num);
 void save_pressure_data(const std::vector<double>& X, const std::vector<double>& Y);
-void save_pressure_evolution(std::fstream& saver, const double Dia, const double Press);
-double media(const double a, const double b);
+void save_pressure_evolution(std::fstream& saver, double Dia, double Press1, double Press2 = 0.0, double Press3 = 0.0);
+double evaluate_trans(const double B1, const double B2);
 void teste_tdma();
 
-constexpr double k_x {10e-15};       // permeabilidade
-constexpr double phi_ref {0.25};     // porosidade
-constexpr double P_ini {4.5e7};      // Pressão inicial
-constexpr double Lx {5000.0};        // dimensão em x
-constexpr double Ly {40.0};          // dimensão em y
-constexpr double Lz {10.0};          // dimensão em z
-constexpr double B0 {1.5};           // B de referência
-constexpr double mu {1.2e-3};        // viscosidade
-constexpr double c_ref {6.0e-10};    // compressibilidade
-constexpr double Vb {Lx*Ly*Lz};      // volume
-constexpr double A_x {Ly*Lz};        // área
-constexpr int N {256};               // número de células
+//Fatores de conversão:
+constexpr double factor_L {0.3048};			// metro <-> pé
+constexpr double factor_mu {0.001};			// Pa.s  <-> cp
+constexpr double factor_kx {0.9869233};		// micrometro quadrado <-> darcy
+constexpr double factor_P {6.894757};		// kPascal <-> psia
+constexpr double factor_c {0.1450377};		// kPascal <-> psia
+constexpr double factor_q {0.1589873};		// std m^3/d <-> STD/D
+constexpr double factor_t {86400};			// segundos <-> dia
+
+// Variáveis do problema e da siomulação:
+constexpr double k_x {0.01};				// permeabilidade (10e-15 m^2
+constexpr double phi_ref {0.25};			// porosidade
+constexpr double P_ini {45000};				// Pressão inicial
+constexpr double Lx {5000.0};				// dimensão em x
+constexpr double Ly {40.0};					// dimensão em y
+constexpr double Lz {10.0};					// dimensão em z
+constexpr double B0 {1.5};					// B de referência
+constexpr double mu {1.2e-3};				// viscosidade
+constexpr double c_ref {6.0e-7};			// compressibilidade
+constexpr double Vb {Lx*Ly*Lz};				// volume
+constexpr double A_x {Ly*Lz};				// área
+constexpr int N {128};						// número de células
 constexpr double dx = Lx/N;
-constexpr int factor {86400};        // fator de conversão segundos/dia
-constexpr double D {400.0/factor};   // vazão no lado esquerdo
+constexpr double D {30.0};					// vazão no lado esquerdo
 constexpr double ti {0.0};
-constexpr double tf {10*365*factor};
-constexpr double dt {0.5*factor};
+constexpr double tf {365*10};
+constexpr double dt {0.5};
 constexpr auto nsteps = static_cast<int>((tf - ti)/dt);
+constexpr double betac {86.4e-06};
 
 int main(int argc, char* argv[]){
 	/*
@@ -49,11 +59,10 @@ int main(int argc, char* argv[]){
 
 	auto Pos = linspace<double>(0.0, Lx, N);                             // vetor para plotar P por x
 	std::vector<double> Pressure (N, P_ini);                             // vetor com as pressões
-	std::vector<std::vector<double>> T (N, std::vector<double>(N, 0.0)); // matriz de transmissibilidades
+	std::vector<std::vector<double>> T (N, std::vector<double>(N, 0.0));// matriz de transmissibilidades
 
 	evaluate_pressure(T, Pressure);
 	save_pressure_data(Pos, Pressure);
-	
 }
 
 void evaluate_pressure(std::vector<std::vector<double>>& Trans, std::vector<double>& P){
@@ -72,7 +81,6 @@ void evaluate_pressure(std::vector<std::vector<double>>& Trans, std::vector<doub
 	time_file << std::setw(10) << "Dias " << std::setw(10) << "Pressao (kPa)" << std::endl;
 	std::fstream time_data{filename, std::ios::out|std::ios::app};
 
-
 	// Iteraçao no tempo:
 	for (size_t n = 1; n <= nsteps; n++){
 
@@ -84,18 +92,19 @@ void evaluate_pressure(std::vector<std::vector<double>>& Trans, std::vector<doub
 			// Contorno esquerdo:
 			if (i == 0){
 				Bi_next = evaluate_B(P[i+1]);
-				Ei = (A_x*k_x)/(dx*mu*media(Bi, Bi_next));
-				Wi = (A_x*k_x)/(dx*mu*media(evaluate_B(P[i] - D*dx), Bi));
+				Bi_prev = evaluate_B(P[i] - D*dx);
+				Ei = betac*evaluate_trans(Bi, Bi_next);
+				Wi = betac*evaluate_trans(Bi, Bi_prev);
 
 				Trans[i][i] = - (Ei + gamma/dt);
-				Trans[i][i+1] = Ei;				
-				P[i] = -P[i]*(gamma/dt) + (Wi*D*dx);
+				Trans[i][i+1] = Ei;
+				P[i] = -P[i]*(gamma/dt) + Wi*(D*dx);
 			}
 
 			// Contorno direito:
 			else if (i == N-1){
 				Bi_prev = evaluate_B(P[i-1]);
-				Wi = (A_x*k_x)/(dx*mu*media(Bi_prev, Bi));
+				Wi = betac*evaluate_trans(Bi_prev, Bi);
 
 				Trans[i][i-1] = Wi;
 				Trans[i][i] = - (Wi + gamma/dt);
@@ -107,31 +116,59 @@ void evaluate_pressure(std::vector<std::vector<double>>& Trans, std::vector<doub
 				Bi_prev = evaluate_B(P[i-1]);
 				Bi_next = evaluate_B(P[i+1]);
 
-				Wi = (A_x*k_x)/(dx*mu*media(Bi_prev, Bi));
-				Ei = (A_x*k_x)/(dx*mu*media(Bi, Bi_next));
+				Wi = betac*evaluate_trans(Bi_prev, Bi);
+				Ei = betac*evaluate_trans(Bi, Bi_next);
 
-				Trans[i][i-1] = Wi;                                      // termo à esquerda
-				Trans[i][i] =  - (Wi + (gamma/dt) + Ei ) ;               // termo na diagonal
-				Trans[i][i+1] = Ei ;                                     // termo à direita
+				Trans[i][i-1] = Wi;                           // termo à esquerda
+				Trans[i][i] =  - (Wi + (gamma/dt) + Ei );     // termo na diagonal
+				Trans[i][i+1] = Ei;                           // termo à direita
 
 				P[i] = - P[i] * (gamma/dt);
 			}
 
 		}
-		// if (n == 1){
-			// print_array_2D(Trans);
-			// std::cout << "Vetor de pressoes" << std::endl;
-			// print_array_1D(P);
-		// }
+		if (n == 1){
+			//print_array_2D(Trans);
+			//std::cout << "Vetor de pressoes" << std::endl;
+			//print_array_1D(P);
+		}
 		P = solve_by_tdma(Trans, P);
 
 		// Registrar a evolução no tempo:
-		save_pressure_evolution(time_data, n*dt, P[1]);
-
-			
+		save_pressure_evolution(time_data, n*dt, P[0], P[static_cast<int>(0.1*N)], P[static_cast<int>(0.2*N)]);
 	}
 }
 
+double evaluate_B(const double p){
+	return B0 / (1.0 + c_ref*(p - P_ini));
+}
+
+double evaluate_trans(const double B1, const double B2){
+	double res1 = (A_x*k_x)/(dx*mu*B1);
+	double res2 = (A_x*k_x)/(dx*mu*B2);
+	return 0.5*(res1 + res2);
+}
+
+void save_pressure_data(const std::vector<double>& X, const std::vector<double>& Y){
+	static int a {1};
+	std::string init {"pressure_data_"};
+	std::string filename = init + std::to_string(a) + ".txt";
+	std::fstream saver{filename, std::ios::out|std::ios::trunc};
+
+	saver << std::setw(10) << "x (m) " << std::setw(10) << "Pressao (kPa)" << std::endl;
+	const auto N = X.size();
+	const auto M = Y.size();
+	if ( N != M)
+		return;
+	for (int i = 0; i < N; i++){
+		saver << std::setw(10) << X[i] << " " << std::setw(10) << std::setprecision(10) << Y[i] << std::endl;
+	}
+	a++;
+}
+
+void save_pressure_evolution(std::fstream& saver, double Dia, double Press1, double Press2, double Press3){
+	saver << std::setw(10) << Dia << " " << std::setw(10) << Press1 << " " << Press2 << " " << Press3 << std::endl;
+}
 
 std::vector<double> solve_by_tdma(const std::vector<std::vector<double>>& Mat, const std::vector<double>& X){
 
@@ -153,7 +190,6 @@ std::vector<double> solve_by_tdma(const std::vector<std::vector<double>>& Mat, c
 		C[i] = Mat[i][j+1];     // diagonal superior
 	}
 	B[n_row-1] = Mat[n_row-1][n_col-1];
-	
 
 	C[0] /= B[0];
 	D[0] /= B[0];
@@ -170,10 +206,6 @@ std::vector<double> solve_by_tdma(const std::vector<std::vector<double>>& Mat, c
 	}
 
 	return D;
-}
-
-double evaluate_B(const double p){
-	return B0 / (1.0 + c_ref*(p - P_ini));
 }
 
 template <typename T>
@@ -199,28 +231,6 @@ void print_array_2D(const std::vector<std::vector<T>> A){
 	}
 }
 
-void save_pressure_data(const std::vector<double>& X, const std::vector<double>& Y){
-	static int a {1};
-	std::string init {"pressure_data_"};
-	std::string filename = init + std::to_string(a) + ".txt";
-	std::fstream saver{filename, std::ios::out|std::ios::trunc};
-
-	saver << std::setw(10) << "x (m) " << std::setw(10) << "Pressao (kPa)" << std::endl;
-	const auto N = X.size();
-	const auto M = Y.size();
-	if ( N != M)
-		return;
-	for (int i = 0; i < N; i++){
-		saver << std::setw(10) << X[i] << " " << std::setw(10) << std::setprecision(10) << Y[i]/1000 << std::endl;
-	}
-	a++;
-}
-
-void save_pressure_evolution(std::fstream& saver, const double Dia, const double Press){
-	saver << std::setw(10) << Dia/factor << " " << std::setw(10) << Press/1000 << std::endl;
-}
-
-
 template <typename T>
 std::vector<T> linspace(const double xi, const double xf, int Num){
 
@@ -236,11 +246,6 @@ std::vector<T> linspace(const double xi, const double xf, int Num){
 	}
 
 	return V;
-}
-
-double media(const double a, const double b){
-	return (a + b)/2.0;
-	//return 1.0/(0.5 * (1.0/a + 1.0/b));
 }
 
 void teste_tdma(){
---
+++ source/tempo_plot.plt
@@ -0,0 +1,18 @@
+#set key outside
+plot "time_evolution_data.txt" using 1:2 title "x = 0 (m)" w l lw 2 lc "blue", "time_evolution_data.txt" using 1:3 title "x = 0.1L (m)" w l lw 2 lc "red",  "time_evolution_data.txt" using 1:4 title "x = 0.2L (m)" w l lw 2 lc "green"
+set terminal pngcairo enhanced
+set output "pressao_no_tempo.png"
+set grid xtics ytics lw 2
+set grid
+set key bottom left
+set xlabel "t (dias)"
+set ylabel "Pressao (kPa)"# rotate by 0
+replot
+set terminal wxt
+set output
+
+
+# with lines = w l
+# linewidth = lw
+# linecolor = lc
+# dashtype = dt
